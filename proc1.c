457a26a8a75b052a93144b05fb025c0efa4a1185
Van <valik.silchenko@gmail.com>
2022-03-26 19:49:18 +0300

    lab5 ver 1.1, fixed errors

--- proc1.c
+++ proc1.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <semaphore.h>
-#include <bits/fcntl.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
 int flag = 0;
-char name[10] = "/synchron";
+char name[11] = "/semaphore";
 
 void* func(void* args) {
-    sem_t* id = sem_open(name, O_CREAT);
-    FILE* fp = fopen("lab5.txt", "w");
+    sem_t* id = sem_open(name, O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP, 1);
+    if (id == SEM_FAILED) {
+        printf("Error: %s\n", strerror(errno));
+        exit(errno);
+    }
+    FILE* fp = fopen("lab5.txt", "a");
     while (flag == 0) {
         sem_wait(id);
         for (int i = 0; i < 10; i++) {
             fputc('1', fp);
             printf("1");
+            fflush(stdout);
             sleep(1);
         }
         sem_post(id);
@@ -33,10 +41,12 @@ int main() {
     printf("Программа 1 ожидает нажатия клавиши.\n");
     getchar();
     printf("Клавиша нажата.\n");
+
+    flag = 1;
     pthread_join(id, NULL);
 
     sem_unlink(name);
 
-    printf("Программа 1 закончила работу.\n");
+    printf("\nПрограмма 1 закончила работу.\n");
     return 0;
 }
--- proc2.c
+++ proc2.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <semaphore.h>
-#include <bits/fcntl.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
 int flag = 0;
-char name[10] = "/synchron";
+char name[11] = "/semaphore";
 
 void* func(void* args) {
-    sem_t* id = sem_open(name, O_CREAT);
-    FILE* fp = fopen("lab5.txt", "w");
+    sem_t* id = sem_open(name, O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP, 1);
+    if (id == SEM_FAILED) {
+        printf("Error: %s\n", strerror(errno));
+        exit(errno);
+    }
+    FILE* fp = fopen("lab5.txt", "a");
     while (flag == 0) {
         sem_wait(id);
         for (int i = 0; i < 10; i++) {
             fputc('2', fp);
             printf("2");
+            fflush(stdout);
             sleep(1);
         }
         sem_post(id);
@@ -33,8 +41,10 @@ int main() {
     printf("Программа 2 ожидает нажатия клавиши.\n");
     getchar();
     printf("Клавиша нажата.\n");
+
+    flag = 1;
     pthread_join(id, NULL);
 
-    printf("Программа 2 закончила работу.\n");
+    printf("\nПрограмма 2 закончила работу.\n");
     return 0;
 }
