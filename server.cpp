9b416966dd377360f0684a96c73cfbec24a411c4
van <valik.silchenko@gmail.com>
2022-04-17 00:53:08 +0300

    ver 0.1

--- server.cpp
+++ server.cpp
@@ -5,31 +5,76 @@
 #include <cstdlib>
 #include <cerrno>
 #include <fcntl.h>
-#include <queue>
-#include <string>
+#include <vector>
 #include <strings.h>
+#include <unistd.h>
 
-int servSoc;
-char name[] = "mysock";
-std::queue<std::string> msgs;
+int servSoc, clientSoc, bufSize = 10;
+char name[] = "mycock";
 bool receivingFl = true, handlingFl = true, awaitingFl = true;
+struct sockaddr_un clAddr;
+socklen_t len;
+pthread_t idAwt, idHndl, idRcv;
+pthread_mutex_t mutex;
+std::vector<int> queue(bufSize);
 
 void* receiving(void* args) {
-
+    long received;
+    uint recvbuf;
+    while (receivingFl) {
+        if ((received = recv(clientSoc, &recvbuf, sizeof(recvbuf), 0)) == -1) {
+            perror("receive");
+            sleep(1);
+        } else if (received == 0) {
+            printf("Разъединение.\n");
+            sleep(1);
+        } else {
+            pthread_mutex_lock(&mutex);
+            queue.insert(queue.begin(), recvbuf);
+            pthread_mutex_unlock(&mutex);
+        }
+    }
     pthread_exit(nullptr);
 }
 
 void* handling(void* args) {
-
+    char temp[50];
+    uint N;
+    while (handlingFl) {
+        pthread_mutex_lock(&mutex);
+        if (!queue.empty()) {
+            printf("Запрос %d принят.\n", queue.back());
+            queue.pop_back();
+            pthread_mutex_unlock(&mutex);
+            getdomainname(temp, 50);
+            N = strlen(temp);
+            if ((send(clientSoc, temp, N, 0)) == -1) {
+                perror("send");
+                sleep(1);
+            } else
+                printf("Отправлено: %s\n", temp);
+        } else {
+            pthread_mutex_unlock(&mutex);
+            sleep(1);
+        }
+    }
     pthread_exit(nullptr);
 }
 
 void* awaiting(void* args) {
     printf("Ожидание соединения.\n");
-    int s;
     while (awaitingFl) {
-        s = accept(servSoc, )
+        if ((clientSoc = accept(servSoc, (sockaddr*)(&clAddr), &len)) == -1) {
+            perror("accept");
+            sleep(1);
+        } else {
+            printf("Соединение установлено.\n");
+            pthread_create(&idRcv, nullptr, receiving, nullptr);
+            pthread_create(&idHndl, nullptr, handling, nullptr);
+            pthread_exit(nullptr);
+        }
     }
+    pthread_exit(nullptr);
 }
 
 int main() {
@@ -49,15 +94,24 @@ int main() {
     bind(servSoc, (struct sockaddr*) &addr, sizeof(struct sockaddr));
     listen(servSoc, 1);
 
-    pthread_t id;
-    pthread_create(&id, nullptr, awaiting, nullptr);
+    pthread_mutex_init(&mutex, nullptr);
+    pthread_create(&idAwt, nullptr, awaiting, nullptr);
 
     printf("Ожидание нажатия клавиши.\n");
     getchar();
     printf("Клавиша нажата.\n");
 
     awaitingFl = false;
-    pthread_join(id, nullptr);
+    receivingFl = false;
+    handlingFl = false;
+    pthread_join(idAwt, nullptr);
+    pthread_join(idRcv, nullptr);
+    pthread_join(idHndl, nullptr);
+    pthread_mutex_destroy(&mutex);
+
+    shutdown(clientSoc, 2);
+    close(servSoc);
+    close(clientSoc);
 
     printf("Серверная программа закончила работу.\n");
     return 0;
